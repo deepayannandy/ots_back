8402c5c2150dc7f38c85ea1264de416ee9ec35a8
deepayannandy <deepayan.622@gmail.com>
2026-02-06 00:20:40 +0530

    fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use("/public/uploads", express.static(process.env.PublicFolderPath));
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "200mb" }));
 
 const health = require("./routes/healthCheck");
 const auth = require("./routes/authentication");
