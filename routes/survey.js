f454988327085678b95e4e5b08273588648db649
Deepayan Nandy <deepayan.622@gmail.com>
2026-03-12 20:52:29 +0530

    bug fix

--- routes/survey.js
+++ routes/survey.js
@@ -59,7 +59,9 @@ router.patch("/updateSurvey/:id", async (req, res) => {
     if (!selectedReactor)
       return res.status(404).json({ error: "Reactor not found" });
     console.log(selectedSurveyReactor);
-    if(selectedSurveyReactor[0].updatedAt- new Date() < 10000){
+     console.log(new Date()-selectedSurveyReactor[0].updatedAt);
+    if(new Date()-selectedSurveyReactor[0].updatedAt < 10000){
+      console.log("Please wait for 10 seconds before updating again");
       return res.status(409).json({ error: "Please wait for 10 seconds before updating again" });
     }
     if (req.body.detection != null) {
