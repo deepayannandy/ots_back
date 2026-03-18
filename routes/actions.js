5c3ac3a6a7ae5e78348bf87b93e4a1a223018ad9
Deepayan Nandy <deepayan.622@gmail.com>
2026-03-19 00:56:20 +0530

    search MQTT

--- routes/actions.js
+++ routes/actions.js
@@ -2,12 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../utils/verifyToken");
+const reactorModel = require("../models/reactorModel");
+const mqttService = require("../utils/mqtt");
 
 router.post("/searchActions", async (req, res) => {
   try {
     const actions = req.body;
     console.log("Getting actions");
     console.log(actions);
+    const reactor = await reactorModel.findById(actions.reactorId);
+    if (!reactor) {
+      return res.status(404).json({ error: "Reactor not found" });
+    }
+    let index = 0;
+    reactor.tubes.forEach((tube, index) => {
+      if (tube.id.toString() === actions.holeId) {
+        mqttService.publish("iiotcore/gotoTube", {
+          tubeIndex: index,
+          tubeId: actions.holeId,
+        });
+      }
+    });
     return res.status(200).json({
       Success: true,
       data: actions,
