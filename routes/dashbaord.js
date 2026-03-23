13716cf6ca662740c5907a780b0f4a508014d94e
Deepayan Nandy <deepayan.622@gmail.com>
2026-03-23 14:35:40 +0530

    bug fix

--- routes/dashbaord.js
+++ routes/dashbaord.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../utils/verifyToken");
 const workOrderModel = require("../models/workOrderModel");
+const reactorModel = require("../models/reactorModel");
 
 router.get("/getDashboardData", verifyToken, async (req, res) => {
   try {
@@ -11,11 +12,34 @@ router.get("/getDashboardData", verifyToken, async (req, res) => {
       .populate("tubeSheet")
       .populate("phaseData.phaseData")
       .exec();
+    const numTubes = 187;
+    const finalData = [];
+    workOrderModels.forEach((wo_1) => {
+      const wo = wo_1.toObject();
+      wo.phaseData.forEach((phase) => {
+        if (phase.phaseData != undefined) {
+          console.log(
+            Math.ceil(
+              ((phase.phaseData.data.length - phase.phaseData.repeat) /
+                numTubes) *
+                100,
+            ),
+          );
+          phase["progress"] = Math.ceil(
+            ((phase.phaseData.data.length - phase.phaseData.repeat) /
+              numTubes) *
+              100,
+          );
+        }
+      });
+      finalData.push(wo);
+    });
     return res.status(200).json({
       Success: true,
-      data: workOrderModels,
+      data: finalData,
     });
   } catch (e) {
+    console.log(e);
     return res.status(500).json({ error: e.message });
   }
 });
